7ca15ad94d5aef6c7b13973cd3cf7b111d0182cf
Tim Batty <tbatty7@gmail.com>
2024-11-12 21:18:17 -0500

    added button and call to http endpoint

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,14 +1,23 @@
-import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable, Input } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-first',
   standalone: true,
   imports: [],
   templateUrl: './first.component.html',
-  styleUrl: './first.component.css'
+  styleUrl: './first.component.css',
 })
 export class FirstComponent {
-  
+  constructor(private http: HttpClient) {}
+
+  @Input() data = 'result will go here'
+
+  async buttonPress() {
+    const response = this.http.get<any>('https://pokeapi.co/api/v2/pokemon/ditto')
+    const result = await firstValueFrom(response)
+    console.log({data: result});
+    this.data = JSON.stringify(result)
+  }
 }
-
-
